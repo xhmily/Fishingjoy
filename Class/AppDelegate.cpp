efdc99181383582f8142446fdaca1705a060f432
孟 <975023381@qq.com>
2017-12-13 21:19:10 +0800

    1213

--- Class/AppDelegate.cpp
+++ Class/AppDelegate.cpp
@@ -14,6 +14,8 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
+// initialize director
+// initialize director
     CCDirector* pDirector = CCDirector::sharedDirector();
     CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
 
@@ -23,6 +25,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
+ // set FPS. the default value is 1.0/60 if you don't call this
+ // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
